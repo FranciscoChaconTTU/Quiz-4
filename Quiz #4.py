f92e48d3a9c7352504d2d4e91ff7ed5553c2c4b2
FranciscoChaconTTU <40004744+FranciscoChaconTTU@users.noreply.github.com>
2018-06-28 15:18:56 -0500

    Create Quiz #4.py

---
+++ Quiz #4.py
@@ -0,0 +1,25 @@
+import math
+def QR_Fact(matrix):
+  if type(matrix) != list:
+    print ("Error")
+    return None
+  if len(matrix)==0:
+    print ("Error")  
+    return None
+  for i in range(len(matrix):
+    if type(matrix) != list:
+      print ("Error")
+      return None
+    if len(matrix)==0:
+      print ("Error")  
+      return None
+    for j in range(len(matrix[i])):
+      if type(matrix[i][j]) !=int:
+        print("Error")
+        return None
+      if type(matrix[i][j]) !=float:
+        print("Error")
+        return None
+      if type(matrix[i][j]) !=complex:
+        print("Error")
+        return None
