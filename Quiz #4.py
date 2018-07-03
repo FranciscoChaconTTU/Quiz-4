a2118ef64b458877363221c480299d6f38adbcdf
FranciscoChaconTTU <40004744+FranciscoChaconTTU@users.noreply.github.com>
2018-07-02 22:11:28 -0500

    Update Quiz #4.py

--- Quiz #4.py
+++ Quiz #4.py
@@ -12,67 +12,75 @@ def twoNorm(vector):
       x = 0
       x = x + (vector[i]**2)
     x = x**(1/2)
-    return x
+  return x
 def normalize(vector):
   "This function takes the max of a vector. This is similar to question 4. The only difference is that this fuction calls function 4 and divides every element in the vector by its max. We use the infNorm to calculate the max value. This fuction returns a vector"
-  #This line is used to called the infNorm that we used before
-  x=twoNorm(vector)
-  y=[]
-  #This line takes the dimension of the vector
-  for i in range(len(vector)):
-    #This line takes the division of each element by the max magnitude and stores it to the blank list
-    y.append(vector[i]/x)
-    #This line returns a vector with the division
-  return y
+   # This variable will keep track of the validity of our input.
+  inputStatus = True  
+   #This line takes the dimension of the vector
+  for i in range(len(vector)):  
+    # This for loop will check each element of the vector to see if it's a number. 
+    if ((type(vector[i]) != int) and (type(vector[i]) != float) and (type(vector[i]) != complex)):
+      inputStatus = False
+      print("Invalid Input")
+    # If the input is valid the function continues
+    if inputStatus == True:
+    #This line is used to called the twoNorm that we used before
+      x=twoNorm(vector)
+      y=[]
+      #This line takes the division of each element by the max magnitude and stores it to the blank list
+      y.append(vector[i]/x)
+      #This line returns a vector with the division
+    return y
 def scalarVecMulti(scalar,vector):
   "This fuction takes in a scalar and multiplies it with a vector. First in takes the lenght of the vector and multiplies the first element of the vector to the scalar and it stores it to the blank list. Then it does this for all elements and returns a vector."
  # This variable will keep track of the validity of our input.
   inputStatus = True  
   # This for loop will check each element of the vector to see if it's a number. 
   for i in range(len(vector)):  
-    if ((type(vector[i]) != int) and (type(vector[i]) != float) and (type(vector[i]) != complex)):
+    if type(vector[i]) != int and type(vector[i]) != float and type(vector[i])!= complex and type(scalar) != int:
       inputStatus = False
       print("Invalid Input")
-    # If the input is valid the function continues
-  if inputStatus == True:
-    x=[]
-    for i in range(len(vector)):
-    #This lines takes the elements of the vector and multiplies if by the scalar respectively and stores it into a blank list.
-      x.append(scalar*vector[i])
+      # If the input is valid the function continues
+    if inputStatus == True:
+      x=[]
+      #This lines takes the elements of the vector and multiplies if by the scalar respectively and stores it into a blank list.
+      x.append(scalar * vector[i])
     #This line returns the vector
-  return x 
+    return x 
 def dot(vector01,vector02):
   "This fuction takes in two vectors and first checks if they are lists. Then it checks if they are of the same dimensions. Then if they are of the same dimensions it multiplies the elements in the same positions and adds them at the end. If they are not of the same dimensions it return invalid input."
+  if len(vector01)==len(vector02):
    # This variable will keep track of the validity of our input.
-  inputStatus = True  
-  # This for loop will check each element of the vector to see if it's a number. 
-  for i in range(len(vector01)): 
-    if ((type(vector01[i])!=int) and (type(vector01[i])!=float) and (type(vector01[i]) !=complex) and len(vector01)==0 or len(vector02)==0):
-      inputStatus = False
-      print("Invalid Input")
+    inputStatus = True  
+    # This for loop will check each element of the vector to see if it's a number. 
+    for i in range(len(vector01)): 
+      if ((type(vector01[i])!=int) and (type(vector01[i])!=float) and (type(vector01[i]) !=complex) and len(vector01)==0 or len(vector02)==0):
+        inputStatus = False
+        print("Invalid Input")
+  else:
+    inputStatus=False
+    print("Wrong Dimensions")
   # If the input is valid the function continues
-  if inputStatus == True and len(vector01)==len(vector02):
+  if inputStatus == True: 
     x = 0
     #If the dimensions are the same go ahead and do the multiplication of each element
     for i in range(len(vector01)):
       #After the multiplication we are adding it to x and giving us the answer at the end as a scalar
       x+=vector01[i]*vector02[i]
     return x
-  #If the dimensions are different print invalid input and return none
-  else:
-    print ("invalid input")
-    return None
 def vecSubtract(vector03,vector04):
   "This fuction takes in two vector of the same dimension and subtracts them. First in checks if the dimension are the same, if they aren't it prints invalid input. If they are the same dimension it takes the first element of the first vector and the first element of the second vector and stores it on a blank matrix. It keeps doing this for all elements in the vector"
-  inputStatus = True 
   if len(vector03)==len(vector04):
-    for i in range(len(vector03)):  
-      if ((type(vector03[i]) != int) and (type(vector03[i]) != float) and (type(vector03[i]) != complex)):
-        inputStatus = False
-        print("Invalid Input")
+    inputStatus = True 
+    for i in range(len(vector03)):
+      for j in range(len(vector04)):
+        if (type(vector03[i]) != int and type(vector04[j])!= int) and (type(vector03[i]) != float and type(vector04)[j]!= float) and (type(vector03[i]) != complex and type(vector04[j])!=complex):
+          inputStatus = False
+          print("Invalid Input")
   else:
     inputStatus=False
-    print("Invalid Input")
+    print("Wrong Dimensions")
   # If the input is valid the function continues to compute the 2-norm
   if inputStatus == True:
     x = []
@@ -82,10 +90,6 @@ def vecSubtract(vector03,vector04):
       print(vector04[i])
       x.append(vector03[i]-vector04[i])    
     return x
-    #This line is to print invalid input if the dimensions aren't the same
-  else:
-    print("invalid input")
-    return None
 def GS(A):
   if type(A) != list and len(A)==0:
     print ("Error1")
@@ -98,19 +102,18 @@ def GS(A):
       if type(A[i][j]) !=int and type(A[i][j]) !=float and type(A[i][j]) !=complex:
         print("Error3")
         return None
-  n=len(A)
-  m=len(A[0])
-  V=A
-  R=[[0]*n for i in range(n)]
-  Q=[[0]*m for i in range(n)]
+  n = len(A)
+  m = len(A[0])
+  V = A
+  R =[[0]*n for i in range(n)]
+  Q =[[0]*m for i in range(n)]
   for i in range(n):
-    R[i][i]=twoNorm(V[i])
-    Q[i]=normalize(V[i])
+    R[i][i] = twoNorm(V[i])
+    Q[i] = normalize(V[i])
     for j in range(i+1,n):
-      R[j][i]=dot(Q[i],V[j])
-      temp= scalarVecMulti(R[j][i],Q[i]) 
-      print(temp)
-      V[j]=vecSubtract(V[j],temp)
+      R[j][i] = dot(Q[i],V[j])
+      temp = scalarVecMulti( R[i][j],Q[i]) 
+      V[j] = vecSubtract(V[j],temp)
     return (Q,R)
 
 A=[[1,0,1],[2,1,0]]
